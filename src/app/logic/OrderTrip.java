1e42a0d0b34afbcb9f2a7ee54920c26cadbb34a2
Jin <32613337+Asstrade@users.noreply.github.com>
2017-12-18 15:33:33 +0800

    update

--- src/app/logic/OrderTrip.java
+++ src/app/logic/OrderTrip.java
@@ -5,6 +5,16 @@ import app.db.TripOrderInfoDAO;
 import app.entity.TripOrderInfo;
 
 public class OrderTrip {
+	
 	public TripOrderInfoDAO dao = new TripOrderInfoDAO();
+	
+	
+	
+	/**查询进行中订单
+	 * @param id 用户id
+	 */
+	public void search(Integer id) {
+		
+	}
+	
 }
-
