efcef54399a82b701af439db8da0aeba7a4dc2f5
Jin <32613337+Asstrade@users.noreply.github.com>
2017-12-18 15:02:33 +0800

    OrderTrip.java

---
+++ src/app/logic/OrderTrip.java
@@ -0,0 +1,9 @@
+package app.logic;
+
+import java.sql.SQLException;
+import app.db.TripOrderInfoDAO;
+import app.entity.TripOrderInfo;
+
+public class OrderTrip {
+	public TripOrderInfoDAO dao = new TripOrderInfoDAO();
+}
