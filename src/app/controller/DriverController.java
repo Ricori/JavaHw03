e1ecc5fc4a8c6c063f8edcca9ab3c9400d96b537
Jin <32613337+Asstrade@users.noreply.github.com>
2017-12-18 19:25:16 +0800

    update

--- src/app/controller/DriverController.java
+++ src/app/controller/DriverController.java
@@ -1,22 +1,39 @@
 package app.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import app.entity.TripInfo;
+import app.logic.PublishTrip;
+
 @Controller
 public class DriverController {
 
 	@RequestMapping(value = "/user_driver")
-	public String userPassenger(HttpServletRequest request,Model model){
+	public String userDriver(HttpServletRequest request,Model model){
 		
-		
-		
+		PublishTrip p = new PublishTrip();
+		ArrayList<TripInfo> allTripOrders = p.serachNyacTripOrder(1);
+		model.addAttribute("allTripOrders",allTripOrders);
 		
 		return "user_driver";
 	}
 	
+	@RequestMapping(value = "/user_driver_wallet")
+	public String userDriverWallet(HttpServletRequest request,Model model){
+
+		return "user_driver_wallet";
+	}
+	
+	@RequestMapping(value = "/user_driver_me")
+	public String userDriverMe(HttpServletRequest request,Model model){
+
+		return "user_driver_me";
+	}
 	
 }
